bb9d09d22219f03287ccd03ab0d0dd964955ce09
Nick Thieling <nick@adrift.io>
2018-01-13 12:24:18 -0600

    Implement default listen port, and ability to pull from env

--- progress_bar.py
+++ progress_bar.py
@@ -8,4 +8,6 @@ def index():
     return render_template('progress_bar.html', progress=22)
 
 if __name__ == '__main__':
-    app.run(debug = True)
+    # bind to env/heroku provided port, otherwise default 5000
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug = True, port=port)
